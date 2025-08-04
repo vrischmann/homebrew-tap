4626e2bd6fcca2979c38e4bb8e8d8826bfac5109
vrischmann <1916079+vrischmann@users.noreply.github.com>
2025-08-04 19:29:37 +0000

    Update tasks to version v2.3.2

--- Formula/tasks.rb
+++ Formula/tasks.rb
@@ -1,15 +1,15 @@
 class Tasks < Formula
   desc "Stateless CLI tool for managing markdown task lists with Unix-friendly commands"
   homepage "https://github.com/vrischmann/tasks"
-  version "2.2.5"
+  version "2.3.2"
   license "MIT"
 
   if Hardware::CPU.intel?
-    url "https://github.com/vrischmann/tasks/releases/download/v2.2.5/tasks_2.2.5_darwin_amd64.tar.gz"
-    sha256 "be526057604b93ef0cae181cd5651831ad3ed7098cc30316d16f6f32a2d04d9d"
+    url "https://github.com/vrischmann/tasks/releases/download/v2.3.2/tasks_2.3.2_darwin_amd64.tar.gz"
+    sha256 "af0456779d0492359e35761888c58deaaa78716ac0110ac0593da0820026d2b9"
   else
-    url "https://github.com/vrischmann/tasks/releases/download/v2.2.5/tasks_2.2.5_darwin_arm64.tar.gz"
-    sha256 "84f8e1126d0b0909ff235f833032413d63cd9f19b190ef9f8e9b0fc201ba301b"
+    url "https://github.com/vrischmann/tasks/releases/download/v2.3.2/tasks_2.3.2_darwin_arm64.tar.gz"
+    sha256 "019e1e4b6072c1182945fc7ade688eadec226ff95e8b9bcf73c5c4fb2bad4212"
   end
 
   def install
