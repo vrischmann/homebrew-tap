dc01a55ba073a2f9b07edd600e748dc957bf82b8
vrischmann <1916079+vrischmann@users.noreply.github.com>
2025-08-04 23:02:19 +0000

    Update tasks to version v2.3.5

--- Formula/tasks.rb
+++ Formula/tasks.rb
@@ -1,26 +1,26 @@
 class Tasks < Formula
   desc "Stateless CLI tool for managing markdown task lists with Unix-friendly commands"
   homepage "https://github.com/vrischmann/tasks"
-  version "2.3.3"
+  version "2.3.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/vrischmann/tasks/releases/download/v2.3.3/tasks_2.3.3_darwin_amd64.tar.gz"
-      sha256 "4bd2688fc1f1347284d35ce442304a476b128086f15dddd83281bf5b0e00a8f7"
+      url "https://github.com/vrischmann/tasks/releases/download/v2.3.5/tasks_2.3.5_darwin_amd64.tar.gz"
+      sha256 "d57db13c3137a3597cfd6fc01afd8f1b09db323cab92f3b97be66405cbbbf05c"
     else
-      url "https://github.com/vrischmann/tasks/releases/download/v2.3.3/tasks_2.3.3_darwin_arm64.tar.gz"
-      sha256 "f97b12ae5c75ce215a1e8c92bb21e8f3720487c393fdcf7690d8ede5b83c8cba"
+      url "https://github.com/vrischmann/tasks/releases/download/v2.3.5/tasks_2.3.5_darwin_arm64.tar.gz"
+      sha256 "14c428fc05abdf7a3160d740d1939a14cdf69c12eae9ba2f3884baaa8344a5bb"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/vrischmann/tasks/releases/download/v2.3.3/tasks_2.3.3_linux_amd64.tar.gz"
-      sha256 "a062e15766bc68b3bec1b2e146eb278058d1e79212235a3002e460d7546ccb54"
+      url "https://github.com/vrischmann/tasks/releases/download/v2.3.5/tasks_2.3.5_linux_amd64.tar.gz"
+      sha256 "9cd215efd0410d3a2fc4a671bfd0a76c9e28d49c8ee6e59b2c4b7ebbc74ace0a"
     elsif Hardware::CPU.arm?
-      url "https://github.com/vrischmann/tasks/releases/download/v2.3.3/tasks_2.3.3_linux_arm64.tar.gz"
-      sha256 "429abdbf3a16a6ed20d351726390fadad2651c5296c3efa8d3ecd329e1f38fe8"
+      url "https://github.com/vrischmann/tasks/releases/download/v2.3.5/tasks_2.3.5_linux_arm64.tar.gz"
+      sha256 "733221ace91bf3934f884faa89b341b1dafa3b5caaeb20d0f3d0c75fe5d73d3f"
     end
   end
 
