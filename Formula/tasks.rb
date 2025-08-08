92324026e3eaf2a6a6a759492369372444e7dcb2
vrischmann <1916079+vrischmann@users.noreply.github.com>
2025-08-08 18:49:41 +0000

    Update tasks to version v2.4.1

--- Formula/tasks.rb
+++ Formula/tasks.rb
@@ -1,26 +1,26 @@
 class Tasks < Formula
   desc "Stateless CLI tool for managing markdown task lists with Unix-friendly commands"
   homepage "https://github.com/vrischmann/tasks"
-  version "2.4.0"
+  version "2.4.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/vrischmann/tasks/releases/download/v2.4.0/tasks_2.4.0_darwin_amd64.tar.gz"
-      sha256 "e7aee0595556b3cdfed80389da2f0da050bb2d89b7cb913c677680a6d4a157e1"
+      url "https://github.com/vrischmann/tasks/releases/download/v2.4.1/tasks_2.4.1_darwin_amd64.tar.gz"
+      sha256 "c3adbd48bc57812b857a5d0999f2a35575dc80a8a0daaf9a50b8ca09dd1e5b31"
     else
-      url "https://github.com/vrischmann/tasks/releases/download/v2.4.0/tasks_2.4.0_darwin_arm64.tar.gz"
-      sha256 "0a9e04cf14d9077004d4998735e3b9034f99a4f5f32c44a0b9506b98b6c00d2f"
+      url "https://github.com/vrischmann/tasks/releases/download/v2.4.1/tasks_2.4.1_darwin_arm64.tar.gz"
+      sha256 "ee408e149ee0eeb1607d8fbe3d01c9af122afc0f902b53e9fb8db10fea98e639"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/vrischmann/tasks/releases/download/v2.4.0/tasks_2.4.0_linux_amd64.tar.gz"
-      sha256 "8f8c4c3baed1881d14f7e84539e4583af67f3afe80cf7793ceb3f973d49be15d"
+      url "https://github.com/vrischmann/tasks/releases/download/v2.4.1/tasks_2.4.1_linux_amd64.tar.gz"
+      sha256 "2f307c95ab7a1984ee250d3c660ba365b69b4d3f953a96d9c560828066f4f9d9"
     elsif Hardware::CPU.arm?
-      url "https://github.com/vrischmann/tasks/releases/download/v2.4.0/tasks_2.4.0_linux_arm64.tar.gz"
-      sha256 "4912654cca3a8438da3660a2611f2ccf8641cca3ec7b547bba70d70993397bd6"
+      url "https://github.com/vrischmann/tasks/releases/download/v2.4.1/tasks_2.4.1_linux_arm64.tar.gz"
+      sha256 "717a13ea70abdc358f5ec89754ec0a7845b4e72fe51d6f9e7386a7fe4acd8c8c"
     end
   end
 
