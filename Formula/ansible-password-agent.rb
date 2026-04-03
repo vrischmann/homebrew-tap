762b0a7bef43291491780614ebc69a7e52e600e5
Vincent Rischmann <vincent@rischmann.fr>
2026-04-04 00:37:05 +0200

    chore(ansible-password-agent): bump version to 1.0.5
    Add dependency on keyutils for Linux since the 'apa' fish function
    requires the keyctl binary to spawn an isolated session keyring.

--- Formula/ansible-password-agent.rb
+++ Formula/ansible-password-agent.rb
@@ -1,20 +1,22 @@
 class AnsiblePasswordAgent < Formula
   desc "Secure credential provider for Ansible vault and become passwords"
   homepage "https://github.com/vrischmann/mytools"
-  version "0.1.0"
+  version "1.0.5"
   license "MIT"
 
+  depends_on "keyutils" if OS.linux?
+
   on_macos do
-    url "https://github.com/vrischmann/mytools/releases/download/v0.1.0/ansible-password-agent-0.1.0-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/vrischmann/mytools/releases/download/v1.0.5/ansible-password-agent-1.0.5-aarch64-apple-darwin.tar.gz"
     sha256 "TODO"
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/vrischmann/mytools/releases/download/v0.1.0/ansible-password-agent-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/vrischmann/mytools/releases/download/v1.0.5/ansible-password-agent-1.0.5-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "TODO"
     elsif Hardware::CPU.arm?
-      url "https://github.com/vrischmann/mytools/releases/download/v0.1.0/ansible-password-agent-0.1.0-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/vrischmann/mytools/releases/download/v1.0.5/ansible-password-agent-1.0.5-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "TODO"
     end
   end
