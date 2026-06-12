b0c83f392bcf01a8e39438a5428998cf67c7ddf6
Vincent Rischmann <vincent@rischmann.fr>
2026-06-13 01:05:22 +0200

    fix(ansible-password-agent): move url and sha256 out of on_macos/on_linux blocks
    The FormulaAudit/ComponentsOrder rule disallows url and sha256 inside on_macos/on_linux blocks in formulae. Moved them to top-level OS conditionals and kept only depends_on in the on_linux block.

--- Formula/ansible-password-agent.rb
+++ Formula/ansible-password-agent.rb
@@ -4,18 +4,18 @@ class AnsiblePasswordAgent < Formula
   version "1.0.5"
   license "MIT"
 
-  depends_on "keyutils" if OS.linux?
-
-  on_macos do
+  if OS.mac?
     url "https://github.com/vrischmann/mytools/releases/download/v1.0.5/ansible-password-agent-1.0.5-aarch64-apple-darwin.tar.gz"
     sha256 "f0bb5da5a5332f5b5664b89a9bdb29dd30afad53300c024d0b9025cdd483b30f"
-  end
-
-  on_linux do
+  elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/vrischmann/mytools/releases/download/v1.0.5/ansible-password-agent-1.0.5-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "1bb86b9be8447bf3c9814eaa561a47b46117b423501b3cb5a03537210749203e"
     end
+  end
+
+  on_linux do
+    depends_on "keyutils"
   end
 
   def install
